aa2ad96bf9e9ef60855c2984d85719a41be7588d
moliveira <moliveira@pinterest.com>
2023-11-22 03:01:49 -0800

    feat: :sparkles: Add CategorySlice

---
+++ src/features/categories/categorySlice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface Category {
+    id: string;
+    name: string;
+    description: null | string;
+    is_active: boolean;
+    deleted_at: null | string;
+    created_at: string;
+    updated_at: string;
+};
+
+const category: Category = {
+    id: "x",
+    name: "Olive",
+    description: "Any description",
+    is_active: true,
+    deleted_at: null,
+    created_at: "2023-09-13T09:35:46.000000Z",
+    updated_at: "2023-09-13T09:35:46.000000Z",
+};
+
+export const initialState = [
+    category,
+    {... category, id: "y", name: "Red"},
+    {... category, id: "z", name: "Blue"},
+    {... category, id: "w", name: "Green"},
+];
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState: initialState,
+    reducers: {
+        createCategory(state, action) {},
+        updateCategory(state, action) {},
+        deleteCategory(state, action) {},
+    },
+});
+
+export default categoriesSlice.reducer;
