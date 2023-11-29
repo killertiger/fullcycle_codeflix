9c0d5564e7060555934f20bddd004dd62b3d6b5a
moliveira <moliveira@pinterest.com>
2023-11-29 03:50:12 -0800

    feat: :sparkles: Add components to CreateCategory

--- src/features/categories/CreateCategory.tsx
+++ src/features/categories/CreateCategory.tsx
@@ -1,9 +1,91 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, FormControl, FormControlLabel, FormGroup, Grid, Link, Paper, Switch, TextField, Typography } from "@mui/material";
+import { Category } from "./categorySlice";
+import { useState } from "react";
 
-export const CategoryCreate = () => (
-    <Box>
-        <Typography variant="h3" component="h1">
-            Category Create
-        </Typography>
-    </Box>
-)
+export const CategoryCreate = () => {
+    const [isdisabled, setIsdisabled] = useState(false);
+    const [category, setCategory] = useState<Category>({
+        id: "",
+        name: "",
+        is_active: false,
+        created_at: "",
+        updated_at: "",
+        deleted_at: "",
+        description: "",
+    });
+
+    const handleChange = (e: any) => { }
+
+    const handleToggle = (e: any) => { }
+
+    return (
+        <Box>
+            <Paper>
+                <Box p={2}>
+                    <Box mb={2}>
+                        <Typography variant="h4">Create Category</Typography>
+                    </Box>
+                </Box>
+
+                <Box p={2}>
+                    <form>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        name="name"
+                                        label="name"
+                                        value={category.name}
+                                        disabled={isdisabled}
+                                        onChange={handleChange} />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <FormControl fullWidth>
+                                    <TextField
+                                        required
+                                        name="description"
+                                        label="description"
+                                        value={category.description}
+                                        disabled={isdisabled}
+                                        onChange={handleChange} />
+                                </FormControl>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <FormGroup>
+                                    <FormControlLabel 
+                                        label="Active"
+                                        control={
+                                            <Switch 
+                                                name="is_active"
+                                                color="secondary"
+                                                onChange={handleToggle}
+                                                checked={category.is_active}
+                                                inputProps={{"aria-label": "controlled"}}
+                                            />
+                                        }
+                                    />
+                                </FormGroup>
+                            </Grid>
+
+                            <Grid item xs={12}>
+                                <Box display="flex" gap={2}>
+                                    <Button variant="contained" component={Link} href="/categories">
+                                        Back
+                                    </Button>
+
+                                    <Button type="submit" variant="contained" color="secondary" disabled={isdisabled}>
+                                        Save
+                                    </Button>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </Box>
+            </Paper>
+        </Box>
+    )
+}
