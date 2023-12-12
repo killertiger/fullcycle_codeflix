d2a0f3c73f92655505a9a7aea02547770e572feb
moliveira <moliveira@pinterest.com>
2023-12-12 03:08:45 -0800

    Improving ListCastMembers

--- src/features/cast/ListCastMembers.tsx
+++ src/features/cast/ListCastMembers.tsx
@@ -1,40 +1,72 @@
 import React, { useEffect, useState } from "react";
-import { useGetCastMembersQuery } from "./castMembersSlice";
+import { useDeleteCastMemberMutation, useGetCastMembersQuery } from "./castMembersSlice";
 import { GridFilterModel, GridPaginationModel } from "@mui/x-data-grid";
-import { Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { enqueueSnackbar, useSnackbar } from "notistack";
 
 export const ListCastMembers = () => {
-    const [page, setPage] = useState(1);
-    const [search, setSearch] = useState("");
-    const [perPage, setPerPage] = useState(10);
-    const [rowsPerPage] = useState([10, 25, 50, 100]);
-    const options = { page, perPage, search };
+    const initialOptions = {
+        page: 1,
+        search: "",
+        perPage: 10,
+        rowsPerPage: [10, 20, 30]
+    }
+
+    const [options, setOptions] = useState(initialOptions);
+    const { enqueueSnackbar } = useSnackbar();
 
     const { data, isFetching, error } = useGetCastMembersQuery(options);
 
+    const [ deleteCastMember, deleteCastMemberStatus ] = useDeleteCastMemberMutation();
+
+    async function handleDeleteCastMember(id: string) {
+        await deleteCastMember({ id });
+    }
+
     function handleOnPageChange(pageModel: GridPaginationModel) {
-        setPage(pageModel.page + 1);
-        setPerPage(pageModel.pageSize);
+        options.page = pageModel.page;
+        options.perPage = pageModel.pageSize;
+        setOptions(options);
     }
 
     function handleFilterChange(filterModel: GridFilterModel) {
         if (filterModel.quickFilterValues?.length) {
             const search = filterModel.quickFilterValues.join("");
-            setSearch(search);
+            options.search = search;
+            setOptions(options);
         } else {
-            setSearch("");
-        }   
+            options.search = "";
+        }
+        setOptions(options);
     }
 
     useEffect(() => {
-        if (error) {
-            console.log(error);
+        if (deleteCastMemberStatus.isSuccess) {
+            enqueueSnackbar("Cast member deleted successfully", { variant: "success" });
         }
-    }, [error]);
+        if(deleteCastMemberStatus.isError) {
+            enqueueSnackbar("Cast member not deleted", { variant: "error" });
+        }
+    }, [deleteCastMemberStatus, enqueueSnackbar]);
 
     if (error) {
         return <Typography variant="h2">Error!</Typography>;
     }
 
-    return <div>ListCastMembers</div>;
+    return (
+        <Box maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Box display="flex" justifyContent="flex-end">
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    component={Link}
+                    to="/cast-members/create"
+                    style={{ marginBottom: "1rem" }}
+                >
+                    New Cast Member
+                </Button>
+            </Box>
+        </Box>
+    );
 }
