4129b30f5a098d80a3d917dc40ad86a1b03b77ea
moliveira <moliveira@pinterest.com>
2024-01-19 02:43:06 -0800

    test: :white_check_mark: Improve GenreList unit test

--- src/features/genre/GenreList.test.tsx
+++ src/features/genre/GenreList.test.tsx
@@ -3,7 +3,7 @@ import { setupServer } from "msw/node";
 import { baseUrl } from "../api/apiSlice";
 import { genreResponse, genreResponsePage2 } from "./mock";
 import { categoryResponse } from "../categories/mock";
-import { renderWithProviders, screen, waitFor } from "../../utils/test-utils";
+import { fireEvent, renderWithProviders, screen, waitFor } from "../../utils/test-utils";
 import { GenreList } from "./GenreList";
 
 const handlers = [
@@ -19,7 +19,7 @@ const handlers = [
         return res(ctx.json(categoryResponse), ctx.delay(150));
     }),
 
-    rest.delete(`${baseUrl}/categories/56dba005-b027-403f-a7d0-1de33e66bba5`, (_, res, ctx) => {
+    rest.delete(`${baseUrl}/genres/56dba005-b027-403f-a7d0-1de33e66bba5`, (_, res, ctx) => {
         return res(ctx.delay(150), ctx.status(200));
     }),
 ]
@@ -37,22 +37,96 @@ describe("GenreList", () => {
     });
 
     it("should render loading state", () => {
-        renderWithProviders(<GenreList/>);
+        renderWithProviders(<GenreList />);
         const loading = screen.getByRole("progressbar");
         expect(loading).toBeInTheDocument();
     });
 
-    it("should render error state" , async () => {
+    it("should render error state", async () => {
         server.use(
             rest.get(`${baseUrl}/genres`, (_, res, ctx) => {
                 return res(ctx.status(500));
             })
         );
 
-        renderWithProviders(<GenreList/>);
+        renderWithProviders(<GenreList />);
         await waitFor(() => {
             const error = screen.getByText("Error fetching genres!");
             expect(error).toBeInTheDocument();
+        });
+    });
+
+    it("should handle OnPageChange", async () => {
+        renderWithProviders(<GenreList />);
+
+        await waitFor(() => {
+            const name = screen.getByText("Italy");
+            expect(name).toBeInTheDocument();
+        });
+
+        const nextButton = screen.getByTestId("KeyboardArrowRightIcon");
+        fireEvent.click(nextButton);
+
+        await waitFor(() => {
+            const name = screen.getByText("Kuwait");
+            expect(name).toBeInTheDocument();
+        });
+    });
+
+    it("should handle filter change", async () => {
+        renderWithProviders(<GenreList />);
+
+        await waitFor(() => {
+            const name = screen.getByText("Italy");
+            expect(name).toBeInTheDocument();
+        });
+
+        const input = screen.getByPlaceholderText("Search…");
+        fireEvent.change(input, { target: { value: "Italy" } });
+
+        await waitFor(() => {
+            const loading = screen.getByRole("progressbar");
+            expect(loading).toBeInTheDocument();
+        });
+    });
+
+    it("should handle delete genre success", async () => {
+        renderWithProviders(<GenreList />);
+
+        await waitFor(() => {
+            const name = screen.getByText("Italy");
+            expect(name).toBeInTheDocument();
+        });
+
+        const deleteButton = screen.getAllByTestId("delete-button")[0];
+        fireEvent.click(deleteButton);
+
+        await waitFor(() => {
+            const name = screen.getByText("Genre deleted successfully");
+            expect(name).toBeInTheDocument();
+        });
+    });
+
+    it("should handle Delete Genre error", async() => {
+        server.use(
+            rest.delete(`${baseUrl}/genres/56dba005-b027-403f-a7d0-1de33e66bba5`, (_, res, ctx) => {
+                return res(ctx.status(500));
+            })
+        );
+
+        renderWithProviders(<GenreList />);
+
+        await waitFor(() => {
+            const name = screen.getByText("Italy");
+            expect(name).toBeInTheDocument();
+        });
+
+        const deleteButton = screen.getAllByTestId("delete-button")[0];
+        fireEvent.click(deleteButton);
+
+        await waitFor(() => {
+            const name = screen.getByText("Genre not deleted");
+            expect(name).toBeInTheDocument();
         });
     });
 });
