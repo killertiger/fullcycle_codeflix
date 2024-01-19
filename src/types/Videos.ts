ee5a5fc1dcc24bdaae1af07f823bc6fdad3017d9
moliveira <moliveira@pinterest.com>
2024-01-19 03:04:33 -0800

    feat: :sparkles: Add Video Type

---
+++ src/types/Videos.ts
@@ -0,0 +1,59 @@
+import { CastMember } from "./CastMembers";
+import { Category } from "./Categories";
+import { Genre } from "./Genre";
+
+export interface Results {
+    data: Video[];
+    links: Links;
+    meta: Meta;
+}
+
+export interface Result {
+    data: Video;
+    links: Links;
+    meta: Meta;
+}
+
+export interface Video {
+    id: string;
+    title: string;
+    description: string;
+    year_launched: number;
+    opened: boolean;
+    rating: string;
+    duration: number;
+    deleted_at?: string;
+    created_at: string;
+    updated_at: string;
+    genres?: Genre[];
+    categories?: Category[];
+    cast_members?: CastMember[];
+    thumb_file_url: string;
+    banner_file_url: string;
+    trailer_file_url: string;
+    video_file_url: string;
+}
+
+export interface Links {
+    first: string;
+    last: string;
+    prev: null;
+    next: string;
+}
+
+export interface Meta {
+    current_page?: number;
+    from?: number;
+    last_page?: number;
+    path?: string;
+    per_page?: number;
+    to?: number;
+    total?: number;
+}
+
+export interface VideoParams {
+    page?: number;
+    perPage?: number;
+    search?: string;
+    isActive?: boolean;
+}
