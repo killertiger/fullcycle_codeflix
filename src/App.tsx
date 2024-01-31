b4acd09cbaf0c0a0f7334f7c2b64e8cdb7c08cd5
moliveira <moliveira@pinterest.com>
2024-01-31 02:39:01 -0800

    feat: :sparkles: Create useAppTheme and useLocalStorage hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,28 @@
-import * as React from 'react';
-import { Box, ThemeProvider } from '@mui/system'
+import { CssBaseline, Typography } from '@mui/material';
+import { Box, ThemeProvider } from '@mui/system';
+import { SnackbarProvider } from 'notistack';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/Header';
-import { CssBaseline, Typography, createTheme } from '@mui/material';
 import { Layout } from './components/Layout';
-import { darkTheme, lightTheme } from './config/theme';
-import { Routes, Route, Link } from 'react-router-dom';
-import { CategoryList } from './features/categories/ListCategory';
-import { CategoryCreate } from './features/categories/CreateCategory';
-import { CategoryEdit } from './features/categories/EditCategory';
-import { SnackbarProvider, enqueueSnackbar } from 'notistack';
-import { ListCastMembers } from './features/cast/ListCastMembers';
 import { CreateCastMember } from './features/cast/CreateCastMember';
 import { EditCastMember } from './features/cast/EditCastMember';
+import { ListCastMembers } from './features/cast/ListCastMembers';
+import { CategoryCreate } from './features/categories/CreateCategory';
+import { CategoryEdit } from './features/categories/EditCategory';
+import { CategoryList } from './features/categories/ListCategory';
 import { GenreCreate } from './features/genre/GenreCreate';
 import { GenreEdit } from './features/genre/GenreEdit';
 import { GenreList } from './features/genre/GenreList';
-import { VideosList } from './features/videos/VideosList';
 import { VideosCreate } from './features/videos/VideosCreate';
 import { VideosEdit } from './features/videos/VideosEdit';
-import { useState, useEffect } from 'react';
+import { VideosList } from './features/videos/VideosList';
+import { useAppTheme } from './hooks/useAppTheme';
 
 export default function App() {
-  const [theme, setTheme] = useState(darkTheme);
-
-  const toggleTheme = () => {
-    const currentTheme = theme.palette.mode === "dark" ? lightTheme : darkTheme;
-    setTheme(currentTheme);
-    localStorage.setItem("theme", currentTheme.palette.mode);
-  }
-
-  useEffect(() => {
-    const currentTheme = localStorage.getItem("theme");
-    if(currentTheme) {
-      setTheme(theme.palette.mode === "dark" ? lightTheme : darkTheme);
-    }
-  }, []);
+  const [currentTheme, toggleCurrentTheme] = useAppTheme();
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <SnackbarProvider
         autoHideDuration={2000}
@@ -50,7 +35,9 @@ export default function App() {
           height: '100vh',
           backgroundColor: (theme) => theme.palette.grey[900],
         }}>
-          <Header toggleTheme={toggleTheme} theme={theme.palette.mode}/>
+          <Header
+            theme={currentTheme.palette.mode === "dark" ? "dark" : "light"}
+            toggleTheme={toggleCurrentTheme} />
           <Layout>
             <h1>Welcome</h1>
             <Routes>
@@ -72,9 +59,9 @@ export default function App() {
               <Route path="/genres/edit/:id" element={<GenreEdit />} />
 
               {/* Videos */}
-              <Route path="/videos" element={<VideosList/>} />
-              <Route path="/videos/create" element={<VideosCreate/>} />
-              <Route path="/videos/edit/:id" element={<VideosEdit/>} />
+              <Route path="/videos" element={<VideosList />} />
+              <Route path="/videos/create" element={<VideosCreate />} />
+              <Route path="/videos/edit/:id" element={<VideosEdit />} />
 
               <Route path="*" element={
                 <Box sx={{ color: "white" }}>
